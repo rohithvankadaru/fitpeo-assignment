2ec711b73c7ebe7fb2e4d36a5695945d31da84d5
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-10-18 16:25:05 +0530

    added logos an d profile photo in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ export const Dashboard = () => {
     return (
         <div className="dashboard flex flex-column justify-between">
             <section className="dashboard-options-container">
-                <div className="flex align-center" style={{marginBlock: '25px'}}>
+                <div className="flex align-center" style={{ marginBlock: '25px' }}>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         height="1em"
@@ -13,28 +13,32 @@ export const Dashboard = () => {
                         fill="white">
                         <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
                     </svg>
-                    <h3 style={{marginLeft: '10px'}}>Dashboard</h3>
+                    <h3 style={{ marginLeft: '10px' }}>Dashboard</h3>
                 </div>
                 <DashboardOption
                     optionName={"Dashboard"}
                     backgroundColor={"#ffffff1a"}
                     displayProp={"none"}
+                    optionLogo={<svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 512 512" fill="whitesmoke" style={{ marginLeft: '4px', marginRight: '15px' }}><path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17v80c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V448h40c13.3 0 24-10.7 24-24V384h40c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z" /></svg>}
                 />
-                <DashboardOption optionName={"Product"} />
-                <DashboardOption optionName={"Customers"} />
-                <DashboardOption optionName={"Income"} />
-                <DashboardOption optionName={"Promote"} />
-                <DashboardOption optionName={"Help"} />
+                <DashboardOption optionName={"Product"} optionLogo={<svg xmlns="http://www.w3.org/2000/svg" height="0.9em" viewBox="0 0 512 512" fill="whitesmoke" style={{ marginLeft: '4px', marginRight: '15px' }}><path d="M234.5 5.7c13.9-5 29.1-5 43.1 0l192 68.6C495 83.4 512 107.5 512 134.6V377.4c0 27-17 51.2-42.5 60.3l-192 68.6c-13.9 5-29.1 5-43.1 0l-192-68.6C17 428.6 0 404.5 0 377.4V134.6c0-27 17-51.2 42.5-60.3l192-68.6zM256 66L82.3 128 256 190l173.7-62L256 66zm32 368.6l160-57.1v-188L288 246.6v188z" /></svg>} />
+                <DashboardOption optionName={"Customers"} optionLogo={<svg xmlns="http://www.w3.org/2000/svg" height="0.9em" viewBox="0 0 448 512" fill="whitesmoke" style={{ marginLeft: '4px', marginRight: '15px' }}><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" /></svg>} />
+                <DashboardOption optionName={"Income"} optionLogo={<svg xmlns="http://www.w3.org/2000/svg" height="0.9em" viewBox="0 0 512 512" fill="whitesmoke" style={{ marginLeft: '4px', marginRight: '15px' }}><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" /></svg>} />
+                <DashboardOption optionName={"Promote"} optionLogo={<svg xmlns="http://www.w3.org/2000/svg" height="0.9em" viewBox="0 0 576 512" fill="whitesmoke" style={{ marginLeft: '4px', marginRight: '15px' }}><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM229.5 173.3l72 144c5.9 11.9 1.1 26.3-10.7 32.2s-26.3 1.1-32.2-10.7L253.2 328H162.8l-5.4 10.7c-5.9 11.9-20.3 16.7-32.2 10.7s-16.7-20.3-10.7-32.2l72-144c4.1-8.1 12.4-13.3 21.5-13.3s17.4 5.1 21.5 13.3zM208 237.7L186.8 280h42.3L208 237.7zM392 256a24 24 0 1 0 0 48 24 24 0 1 0 0-48zm24-43.9V184c0-13.3 10.7-24 24-24s24 10.7 24 24v96 48c0 13.3-10.7 24-24 24c-6.6 0-12.6-2.7-17-7c-9.4 4.5-19.9 7-31 7c-39.8 0-72-32.2-72-72s32.2-72 72-72c8.4 0 16.5 1.4 24 4.1z" /></svg>} />
+                <DashboardOption optionName={"Help"} optionLogo={<svg xmlns="http://www.w3.org/2000/svg" height="0.9em" viewBox="0 0 512 512" fill="whitesmoke" style={{ marginLeft: '4px', marginRight: '15px' }}><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" /></svg>} />
             </section>
 
             <div className="dashboard-profile flex justify-between align-center">
-                <div>
-                    <h3>Evano</h3>
-                    <p>Project Manager</p>
+                <div className="flex align-center">
+                    <img className="profile-img" src="https://render.fineartamerica.com/images/rendered/search/print/6.5/8/break/images/artworkimages/medium/1/young-girl-reading-jeanhonore-fragonard.jpg" alt="" />
+                    <div>
+                        <h3>Evano</h3>
+                        <p>Project Manager</p>
+                    </div>
                 </div>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    height="1em"
+                    height="0.9em"
                     viewBox="0 0 512 512"
                     fill="whitesmoke"
                 >
